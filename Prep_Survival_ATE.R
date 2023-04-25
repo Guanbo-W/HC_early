989e1ab888d70cd931eb5dbe985cc564ea558c55
Guanbo-W <guanbo.wang@mail.mcgill.ca>
2023-04-24 23:07:21 -0400

    Update Prep_Survival_ATE.R

--- Prep_Survival_ATE.R
+++ Prep_Survival_ATE.R
@@ -4,25 +4,29 @@
 
 ######
 ############ load library
-library(dummies)
+#library(dummies)
 library(MatchIt)
 library(optmatch)
 library(doParallel)
 library(foreach)
 library(fastDummies)
-library(tinytex)
-library(coda)
-library(rjags)
-library(ggplot2)
-require(gridExtra)
+#library(tinytex)
+#library(coda)
+#library(rjags)
+#library(ggplot2)
+#require(gridExtra)
 library(survival)
+library(rjags)
 set.seed(2021)
 
-Nsim=500
-
+Nsim=100
+Ncore=7
+n.chains=3
+n.adapt=1000
+n.iter=2000
 ###### 
 ############ Sample size
-nT=16; nT.w=40; nC=22; nHC0=145; nHC1=467 
+nT=16; nT.w=30; nC=22; nHC0=145; nHC1=467 
 N=nT+nC+nHC0+nHC1
 N.w=nT.w+nC+nHC0+nHC1
 ############ Arm
@@ -37,18 +41,18 @@ A.w=relevel(factor(c(A_MT.w, A_MC, A_HC0, A_HC1)),ref="C")
 DA.w=dummy_cols(A.w)
 
 ############ Conditional Arm effect 
-# beta (log HR)=c(HC0, HC1, T)
+# beta (log OR/log HR)=c(HC0, HC1, T)
 # Scenario 1 (All equivalent)
 beta1=c(log(1), log(1), log(1))
 
-# New Scenario 2 (HC0=HC1=C<T)
-beta2=c(log(1), log(1), log(1/2))
+# Scenario 2 (HC0=HC1=C<T)
+beta2=c(log(1), log(1), log(2))
 
-# New Scenario 3 (HC0=HC1<C<T)
-beta3=c(log(3), log(3), log(1/2))
+# Scenario 3 (HC0=HC1<C<T)
+beta3=c(log(0.7), log(0.7), log(2))
 
-# New Scenario 4  (HC0<HC1<C<T)
-beta4=c(log(12), log(3), log(1/2))
+# Scenario 4  (HC0<HC1<C<T)
+beta4=c(log(0.5), log(0.7), log(2))
 
 beta=cbind(beta1,beta2,beta3,beta4)
 
@@ -154,37 +158,26 @@ X.w=X_gen(nT.w,nC,nHC0,nHC1)
 ############ Confounder coefficient
 alpha=seq(0.01,0.07,0.005)
 ############ EM coefficient
-gamma=0.01
+gamma=0.05
 ############ beta0
 beta0=0.2
 
 ######
 ############ Estimate true ATT: marginal OR(T/C)
+
+# ### Generating survival outcomes
 Gen_Y=function(N, arm, con_effect, cov, nT){
   v=2
   A=-log(runif(N,0,1))
   B= exp(as.matrix(arm[,3:5]) %*% con_effect + cov %*% alpha + cov[, 8]*arm[,5]*gamma)
   Y=(A/B)^(1/v)
   Y1=Y[1:nT];Y2=Y[(nT+1):(nT+nC)];Y3=Y[(nT+nC+1):(nT+nC+nHC0)];Y4=Y[(nT+nC+nHC0+1):(nT+nC+nHC0+nHC1)]
-  C1=rnorm(nT,mean(1.5*Y1),0.1);c1=ifelse(C1>Y1,1,0)
-  C2=rnorm(nC,mean(1.5*Y2),0.1);c2=ifelse(C2>Y2,1,0)
-  C3=rnorm(nHC0,mean(1.5*Y3),0.1);c3=ifelse(C3>Y3,1,0)
-  C4=rnorm(nHC1,mean(1.5*Y4),0.1);c4=ifelse(C4>Y4,1,0)
+  C1=rnorm(nT,mean(Y1),0.1);c1=ifelse(C1>Y1,1,0)
+  C2=rnorm(nC,mean(Y2),0.1);c2=ifelse(C2>Y2,1,0)
+  C3=rnorm(nHC0,mean(Y3),0.1);c3=ifelse(C3>Y3,1,0)
+  C4=rnorm(nHC1,mean(Y4),0.1);c4=ifelse(C4>Y4,1,0)
   return(Surv(Y,c(c1,c2,c3,c4)))
 }
-# ### Generating survival outcomes
-# Gen_Y=function(N, arm, con_effect, cov, nT){
-#   v=2
-#   A=-log(runif(N,0,1))
-#   B= exp(as.matrix(arm[,3:5]) %*% con_effect + cov %*% alpha + cov[, 8]*arm[,5]*gamma)
-#   Y=(A/B)^(1/v)
-#   Y1=Y[1:nT];Y2=Y[(nT+1):(nT+nC)];Y3=Y[(nT+nC+1):(nT+nC+nHC0)];Y4=Y[(nT+nC+nHC0+1):(nT+nC+nHC0+nHC1)]
-#   C1=rnorm(nT,mean(Y1),0.1);c1=ifelse(C1>Y1,1,0)
-#   C2=rnorm(nC,mean(Y2),0.1);c2=ifelse(C2>Y2,1,0)
-#   C3=rnorm(nHC0,mean(Y3),0.1);c3=ifelse(C3>Y3,1,0)
-#   C4=rnorm(nHC1,mean(Y4),0.1);c4=ifelse(C4>Y4,1,0)
-#   return(Surv(Y,c(c1,c2,c3,c4)))
-# }
 
 Gen_Y_Bayes=function(N, arm, con_effect, cov, nT){
   v=2
@@ -200,47 +193,45 @@ Gen_Y_Bayes=function(N, arm, con_effect, cov, nT){
 }
 
 # #hist(Gen_Y(N, DA, beta[,2],X))
-#
-#
-# # survival small MT ATT
-
+# 
+# 
+# # # survival small MT ATT
+# 
 # Gen_Y_ATT=function(AA, X){
 #   v=2
 #   A=-log(runif(length(AA),0,1))
-#   B=exp(as.matrix(AA*log(1/2) + X %*% alpha + X[, 8]*AA*gamma))
+#   B=exp(as.matrix(AA*log(2) + X %*% alpha + X[, 8]*AA*gamma))
 #   Time=(A/B)^(1/v)
 #   return(Time)}
 # 
 # samplenumber=200
 # result=NULL
-# registerDoParallel(16)
-#  result=foreach (i=1:100000, .combine=rbind) %dopar% {
+# for(i in 1:100000){
 #   X=X_gen(nT,nC,nHC0,nHC1)
-#   index=sample(seq(1,(nT+nC),1),samplenumber,replace=TRUE)
+#   index=sample(seq(1,nT,1),samplenumber,replace=TRUE)
 #   Y0=Gen_Y_ATT(rep(0,samplenumber),X[index,])
 #   Y1=Gen_Y_ATT(rep(1,samplenumber),X[index,])
 #   new.Y=c(Y0,Y1);new.trt=c(rep(0,samplenumber),rep(1,samplenumber))
 #   result[i]=summary(coxph(Surv(new.Y)~new.trt))$coef[1]
 # }
 # result=mean(result)
-# # #
-result=-0.6810789
+# #  
+result=0.7052412
 ATT.surv=c(0,result,result,result)
-#
-# survival big MT
+# 
+# # survival big MT
 # samplenumber=200
 # result=NULL
-# registerDoParallel(16)
-# result=foreach (i=1:100000, .combine=rbind) %dopar% {
+# for(i in 1:100000){
 #   X.w=X_gen(nT.w,nC,nHC0,nHC1)
-#   index=sample(seq(1,(nT.w+nC),1),samplenumber,replace=TRUE)
+#   index=sample(seq(1,nT.w,1),samplenumber,replace=TRUE)
 #   Y0=Gen_Y_ATT(rep(0,samplenumber),X.w[index,])
 #   Y1=Gen_Y_ATT(rep(1,samplenumber),X.w[index,])
 #   new.Y=c(Y0,Y1);new.trt=c(rep(0,samplenumber),rep(1,samplenumber))
 #   result[i]=summary(coxph(Surv(new.Y)~new.trt))$coef[1]
 # }
 # result=mean(result)
-result=-0.6807652
+result=0.7040959
 ATT.surv.w=c(0,result,result,result)
 
 
@@ -262,4 +253,3 @@ extS=c(rep(1,nT+nC),rep(2,nHC0+nHC1))
 extS.w=c(rep(1,nT.w+nC),rep(2,nHC0+nHC1))
 trt=c(rep(1,nT),rep(0,N-nT))
 trt.w=c(rep(1,nT.w),rep(0,N.w-nT.w))
-
